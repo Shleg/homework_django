e472849bfec77664eafe232a2a819035ce56c619
Andrew Shlegel <a.shlegel@outlook.com>
2024-04-16 10:18:57 -0300

    Homework 1 is finished

---
+++ homework_project/hw1_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
+# Create your views here.
+def index(request):
+    html = """
+        <div style='margin: 0 auto; text-align: center;'>
+            <h1>Home page</h1>
+            <a href = '/about/'>About us</a>
+        </div>
+    """
+    logger.info('Обработана страница Home page')
+    return HttpResponse(html)
+
+
+def about(request):
+    html = """
+        <div style='margin: 0 auto; text-align: center;'>
+            <h1>About us</h1>
+            <a href = '/'>Home page</a>
+        </div>
+    """
+    logger.info('Обработана страница About us')
+    return HttpResponse(html)
