0dabaf73d5e99e91dbfc87aefbf6c1c5d08c3901
Andrew Shlegel <a.shlegel@outlook.com>
2024-05-09 13:30:48 -0300

    Readonli fields added

--- homework_project/hw5_app/admin.py
+++ homework_project/hw5_app/admin.py
@@ -10,16 +10,19 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'client', 'total_amount', 'order_date')
     inlines = [OrderItemInline]
+    readonly_fields = ('total_amount', 'order_date')
 
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'address', 'registration_date')
     search_fields = ('name', 'email')
+    readonly_fields = ('registration_date',)
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'date_added', 'image')
     search_fields = ('name',)
+    readonly_fields = ('date_added',)
 
 
 admin.site.register(Client, ClientAdmin)
